68b85e1690fc50aed13cffb9c8ba8decabae78ca
Vinay Godaba <godabavinay@gmail.com>
2019-07-08 14:52:53 +0530

    added onGenerateRoute

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -4,6 +4,11 @@ import '../product_manager.dart';
 import './products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
+  final List<Map<String, String>> products;
+  final Function addProduct;
+  final Function deleteProduct;
+
+  ProductsPage(this.products, this.addProduct, this.deleteProduct);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +31,7 @@ class ProductsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('List'),
       ),
-      body: ProductManager(),
+      body: ProductManager(products, addProduct, deleteProduct),
     );
   }
 }
--- lib/products.dart
+++ lib/products.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import './pages/product.dart';
-
 class Products extends StatelessWidget {
   final List<Map<String, String>> products;
   final Function deleteProduct;
@@ -22,14 +20,9 @@ class Products extends StatelessWidget {
               FlatButton(
                 child: Text('Details'),
                 onPressed: () {
-                  Navigator.push<bool>(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ProductPage(
-                          products[index]['title'],
-                          products[index]['imageUrl']
-                        )),
-                  ).then((bool value) {
+                  Navigator.pushNamed<bool>(
+                          context, '/product/' + index.toString())
+                      .then((bool value) {
                     if (value) {
                       deleteProduct(index);
                     }
