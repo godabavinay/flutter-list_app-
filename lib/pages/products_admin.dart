f7818dab99cc61edbc5fc9df42b3b1eadb1cefee
Vinay Godaba <godabavinay@gmail.com>
2019-07-08 13:15:24 +0530

    added tabs in the Products admin page

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,37 +1,57 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_create.dart';
+import './product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('All Products'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => ProductsPage(),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Manage Products'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                child: Text('Create Product'),
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                child: Text('My Products'),
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text('Choose'),
-            ),
-            ListTile(
-              title: Text('All Products'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => ProductsPage(),
-                  ),
-                );
-              },
-            )
+            ProductCreate(),
+            ProductList(),
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: Text('Manage Products'),
-      ),
-      body: Center(
-        child: Text('Manage your Products'),
       ),
     );
   }
